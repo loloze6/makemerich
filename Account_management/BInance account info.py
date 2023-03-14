23682ccafca9f4c73008e89f597f33028e86ba0f
loloze6 <alauze.jeremy@gmail.com>
2023-03-14 19:13:13 +0100

    bracket

--- Account_management/BInance account info.py
+++ Account_management/BInance account info.py
@@ -1,23 +1,28 @@
 import ccxt
 from Account_management.GetAPIKeys import GetAPIKeys
-
-def initiate_exchange(sandbox=True):
-    credential = GetAPIKeys().getAPIkeys(sandbox)
-    binance_api_key = credential['User']
-    binance_api_secret = credential['Password']
-
-    exchange=ccxt.binance()
+def initiate_exchange(exchange='binance', sandbox=True):
+    credential = GetAPIKeys().getAPIkeys(exchange, sandbox)
+    api_key = credential['User']
+    api_secret = credential['Password']
+    if exchange=='binance':
+        exchange=ccxt.binance()
+    elif exchange=='bybit':
+        exchange=ccxt.bybit()
+    else:
+        return('Exchange not supported')
     exchange.set_sandbox_mode(sandbox)
-    exchange.apiKey=binance_api_key
-    exchange.secret=binance_api_secret
+    exchange.apiKey=api_key
+    exchange.secret=api_secret
     exchange.options = {'adjustForTimeDifference': True,
                         'newOrderRespType': 'FULL',
-                        'defaultTimeInForce': 'GTC'}
+                        'defaultTimeInForce': 'GTC',
+                        'timeDifference': 0,
+                        'recvWindow': 5*1000
+                        }
     return exchange
 
-#Not perfect, can fail due to limits
-def convertAllToToken(sandbox=True, token='USDT'):
-    exchange=initiate_exchange(sandbox)
+def convertAllToToken(exchange='binance', sandbox=True, token='USDT'):
+    exchange=initiate_exchange(exchange, sandbox)
     balance= exchange.fetch_balance()
     markets=exchange.loadMarkets()
     basecoin=token
@@ -25,22 +30,52 @@ def convertAllToToken(sandbox=True, token='USDT'):
         if key!=basecoin and value > 0:
             market=key+'/'+basecoin
             try:
-                for items in markets[market]['info']['filters']:
-                    if items['filterType']=='MARKET_LOT_SIZE':
-                        maxvalue=float(items['maxQty'])
-                if value>maxvalue: value=maxvalue
-                transaction=exchange.create_order(market, type='market', side='sell', amount=value)
-                print ('For {} - Transaction {} of {} {} at {} {}'.format(market, transaction['status'], transaction['filled'],key,transaction['average'], basecoin))
+                if exchange=='binance':
+                    for items in markets[market]['info']['filters']:
+                        if items['filterType']=='MARKET_LOT_SIZE':
+                            maxvalue=float(items['maxQty'])
+                            if value>maxvalue: value=maxvalue
+                            transaction=exchange.create_order(market, type='market', side='sell', amount=value)
+                            print ('For {} - Transaction {} of {} {} at {} {}'.format(market, transaction['status'], transaction['filled'],key,transaction['average'], basecoin))    
+                
+                else:
+                    transaction=exchange.create_order(market, type='market', side='sell', amount=value)
+                    print ('For {} - Transaction {} of {} {} at {} {}'.format(market, transaction['status'], transaction['filled'],key,transaction['average'], basecoin))
+            
             except Exception as e:
                 print ('For {} - Transaction could not be completed due to error: {}'.format(market, e))
     newBalance= exchange.fetch_balance()
     print(newBalance['free'])
     
 
-convertAllToToken(sandbox=True, token='USDT')
 
 
+exchange='binance'
+sandbox=True
+exchange=initiate_exchange(exchange, sandbox)
 
-
-exchange=initiate_exchange(sandbox=True)
 exchange.fetch_balance()
+
+exchange.cancel_all_orders('BTC/USDT')
+exchange.fetch_open_orders('BTC/USDT')
+
+
+print(exchange.load_markets()['BTC/USDT'].keys())
+print(exchange.load_markets()['BTC/USDT']['info'])
+
+exchange.create_order(symbol='BTC/USDT', type='MARKET', side='buy',
+                                          amount=0.190127)
+
+exchange.create_order(symbol='BTC/USDT', type='LIMIT', side='sell',
+                                          amount=0.190127, price=27374)
+
+exchange.create_order(symbol='BTC/USDT', type='STOP_LOSS_LIMIT', side='sell',
+                                          amount=0.190127, price=22374, params={'stopPrice': 22374})
+
+
+exchange.create_order(symbol='BTC/USDT', type='LIMIT', side='sell', price=22000, amount=0.1)
+
+convertAllToToken('binance', sandbox, 'USDT')
+
+
+
